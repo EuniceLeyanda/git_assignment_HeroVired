39d9746bcd6f4c19a522c2c4e5ba2c898f3651cc
Eunice Leyanda <euniceleyanda1996@gmail.com>
2024-02-03 16:12:10 +0530

    adding new method to calculate area using diameter

--- GeometryCalculator.py
+++ GeometryCalculator.py
@@ -1,8 +1,11 @@
 import math
 
 class GeometryCalculator:
-    def calculate_circle_area(self, radius):
+    def calculate_circle_area1(self, radius):
         return math.pi * radius ** 2
+    
+    def calculate_circle_area2(self, diameter):
+        return math.pi * radius
 
     def calculate_rectangle_area(self, length, width):
         return length * width
@@ -13,10 +16,14 @@ if __name__ == "__main__":
     # TODO: Implement the feature to calculate the area of a circle
 
     radius = 5
-    print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
+    print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area1(radius)}")
+
+    diameter = 6
+    print(f"The area of the circle with radius {diameter} ={calculator.calculate_circle_area2(diameter)}")
+
 
     # TODO: Implement the feature to calculate the area of a rectangle # length = 10
 
-    # width = 6
-
-    # print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
+    length = 5
+    width = 6
+    print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
